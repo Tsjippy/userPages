fdf26e44964667b4221340db5f17e8e38d6f728f
Tsjippy <ewaldha@hotmail.com>
2024-10-04 13:50:59 +0100

    changed: readme

--- blocks/user_description/build/index.asset.php
+++ blocks/user_description/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ef1e9c89dcee53657f03');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'aff42a2065730ba42591');
--- php/__module_menu.php
+++ php/__module_menu.php
@@ -15,26 +15,6 @@ add_filter('sim_submenu_description', function($description, $moduleSlug){
 	}
 
 	ob_start();
-	?>
-	<p>
-		This module adds 3 shortcodes:
-		<h4>all_contacts</h4>
-		This shortcode displays a button to download all registered users to store them as contacts in gmail or outtlook.<br>
-		Use like this: <code>[all_contacts']</code>
-		<br>
-		<h4>user_link</h4>
-		This shortcode displays a user in a post or page.<br>
-		It has 5 properties:<br>
-		<ul>
-			<li>'id' The id of the user to be displayed, mandatory</li>
-			<li>'picture' Whether the users picture should be displayed or not.</li>
-			<li>'phone' Whether the users phonenumbers should be displayed or not.</li>
-			<li>'email' Whether the users e-mail addresses should be displayed or not.</li>
-			<li>'style' Any additional html styling.</li>
-		</ul>
-		Use like this: <code>[user_link id="12"]</code>
-		<br>
-		<?php
 	$url		= SIM\ADMIN\getDefaultPageLink($moduleSlug, 'allcontacts_pages');
 	if(!empty($url)){
 		?>
