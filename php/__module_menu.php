7543cdc829d24eda7c0a679b9e4836c51cf79023
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2024-10-17 13:35:40 +0100

    Update __module_menu.php

--- php/__module_menu.php
+++ php/__module_menu.php
@@ -2,7 +2,7 @@
 namespace SIM\USERPAGES;
 use SIM;
 
-const MODULE_VERSION		= '8.05';
+const MODULE_VERSION		= '8.0.5';
 //module slug is the same as grandparent folder name
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
